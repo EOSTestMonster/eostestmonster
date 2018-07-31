84d7a7d93bdf90ee685397276f64ff4b724eff23
eos <eos@eosdeAir.lan>
2018-07-31 16:11:52 +0800

    new test

---
+++ testcases/01_get_request.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+from __future__ import unicode_literals
+import os
+import sys
+import time
+import json
+import requests
+import argparse
+import subprocess 
+import traceback
+import multiprocessing
+from decimal import Decimal, getcontext
+
+
+#get info 
+
+def get_info():
+    print subprocess.Popen("cleos get info")
+
+#get block
+
+
+
+def main():
+    if len(sys.argv)<1:
+        print('ERROR:Please supply the param file')
+        return False
+    param_dict, param_str = None, ''
+    with open(sys.argv[1], 'r') as fp:
+        param_str = fp.read()
+        param_dict = json.loads(param_str)
+    if not param_dict:
+        print('ERROR:param file can NOT be empty')
+        return False
+    print('Get params:', sys.argv[0], param_str)
+    return True
+
+if __name__ == '__main__':
+    sys.exit( 0 if main() else 1)
