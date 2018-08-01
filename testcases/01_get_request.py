6b2362d25398b04cd4ca5fdff0c5b307fecdd017
root <978663747@qq.com>
2018-08-01 07:12:14 +0000

    增加测试测试用例

--- testcases/01_get_request.py
+++ testcases/01_get_request.py
@@ -8,20 +8,66 @@ import time
 import json
 import requests
 import argparse
-import subprocess 
+from subprocess import Popen, PIPE 
 import traceback
 import multiprocessing
 from decimal import Decimal, getcontext
 
 
+
 #get info 
 
 def get_info():
-    print subprocess.Popen("cleos get info")
+    cmdline = "cleos get info"
+    process = Popen(cmdline, stdout=PIPE, stderr=PIPE,shell=True)
+    stdout, stderr = process.communicate()
+    if not stderr:
+        print(f'{cmdline} ===========================  ok')
+        return True
+    else:
+        print(f'{cmdline} =========================== fail')
+        print(stderr)
+        return False 
 
-#get block
-
-
+#system contract
+def system_contract():
+    #reprod
+    cmdline = "cleos system regproducer zhangshiqi11 EOS5NKY5vhyqWNgKeNbiUx2iC6cEnooiQsYQMjCmFFMoVLUExGHba https://www.eos.store 900"
+    process = Popen(cmdline, stdout=PIPE, stderr=PIPE,shell=True)
+    time.sleep(2)
+    cmdline1 = "cleos get table eosio eosio producers -l 100"
+    process = Popen(cmdline1, stdout=PIPE, stderr=PIPE,shell=True)
+    stdout, stderr = process.communicate()
+    if not stderr:
+        result = json.loads(stdout)
+        pro = result['rows']
+        for i in pro:
+             if i['owner'] == 'zhangshiqi11':
+                 print(f'{cmdline} =========================== ok')
+                 res = True
+    else:
+        print(f'{cmdline} =========================== fail')
+        print(stderr)
+        res = False 
+    
+    cmdline = "cleos system unregprod zhangshiqi11"
+    process = Popen(cmdline, stdout=PIPE, stderr=PIPE,shell=True)
+    time.sleep(2)
+    cmdline1 = "cleos get table eosio eosio producers -l 100"
+    process = Popen(cmdline1, stdout=PIPE, stderr=PIPE,shell=True)
+    stdout, stderr = process.communicate()
+    if not stderr:
+        result = json.loads(stdout)
+        pro = result['rows']
+        for i in pro:
+            if i['owner'] == 'zhangshiqi11':
+                print(f'{cmdline} =========================== ok')
+                res = True
+    else:
+        print(f'{cmdline} =========================== fail')
+        print(stderr) 
+        res = False 
+    return res    
 
 def main():
     if len(sys.argv)<1:
@@ -34,6 +80,8 @@ def main():
     if not param_dict:
         print('ERROR:param file can NOT be empty')
         return False
+    get_info()
+    system_contract()
     print('Get params:', sys.argv[0], param_str)
     return True
 
