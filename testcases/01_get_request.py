eeeef098b2d6a3736c7181dc8fe5a20993e96a9f
root <978663747@qq.com>
2018-08-02 06:40:24 +0000

    增加创建随机账户

--- testcases/01_get_request.py
+++ testcases/01_get_request.py
@@ -9,15 +9,24 @@ import json
 import string
 import requests
 import argparse
+import random
 from subprocess import Popen, PIPE 
 import traceback
 import multiprocessing
 from decimal import Decimal, getcontext
 
 
+creator = 'zhangshiqi12'
+
+def account_random():
+    seed = "12345abcdefghijklmnopqrstuvwxyz"
+    sa = []
+    for i in range(12):
+        sa.append(random.choice(seed))
+    res = ''.join(sa)
+    return res
 
 #get info 
-
 def get_info():
     cmdline = "cleos get info"
     process = Popen(cmdline, stdout=PIPE, stderr=PIPE,shell=True)
@@ -36,8 +45,24 @@ def transfer():
     account1_balance2 = 0
     account2_balance1 = 0
     account2_balance2 = 0
+    #创建一个账户
+    #newaccount = account_random()
+    newaccount = 'zhangshiqi11'
+    print('new account :',newaccount)
+    #cmdline = f'cleos system newaccount --stake-net \"5 EOS\" --stake-cpu \"5 EOS\" --buy-ram \"2 EOS\"  {creator} {newaccount}   '
+    #process = Popen(cmdline, stdout=PIPE, stderr=PIPE,shell=True)
+    #time.sleep(1)
+    #cmdlinel = f"cleos get account {newaccount}"
+    #process = Popen(cmdlinel, stdout=PIPE, stderr=PIPE,shell=True)
+    #stdout, stderr = process.communicate()
+    #if stdout:
+    #    print(f'{cmdline} ===========================  ok')
+    #if stderr:
+    #    print(f'{cmdline} =========================== fail')
+    #    print(stderr)
+    
     #获取转账钱的=余额
-    cmdline = "cleos get currency balance eosio.token zhangshiqi12"
+    cmdline = f"cleos get currency balance eosio.token {creator}"
     process = Popen(cmdline, stdout=PIPE, stderr=PIPE,shell=True)
     stdout, stderr = process.communicate()
     if stdout:
@@ -48,7 +73,7 @@ def transfer():
         print(f'{cmdline} =========================== fail')
         print(stderr)
     
-    cmdline = "cleos get currency balance eosio.token zhangshiqi11"
+    cmdline = f"cleos get currency balance eosio.token  {newaccount} "
     process = Popen(cmdline, stdout=PIPE, stderr=PIPE,shell=True)
     stdout, stderr = process.communicate()
     if stdout:
@@ -59,7 +84,7 @@ def transfer():
         print(f'{cmdline} =========================== fail')
         print(stderr)
     #执行转账
-    cmdline2 = 'cleos transfer zhangshiqi12 zhangshiqi11 \"2.0000 EOS\" '
+    cmdline2 = f'cleos transfer {creator} {newaccount}  \"2.0000 EOS\" '
     process = Popen(cmdline2, stdout=PIPE, stderr=PIPE,shell=True)
     #stdout, stderr = process.communicate()
     #if stdout:
@@ -69,7 +94,7 @@ def transfer():
     #    print(stderr)
     time.sleep(3)
     #查看转账后的余额
-    cmdline = "cleos get currency balance eosio.token zhangshiqi12"
+    cmdline = f"cleos get currency balance eosio.token {creator} "
     process = Popen(cmdline, stdout=PIPE, stderr=PIPE,shell=True)
     stdout, stderr = process.communicate()
     if stdout:
@@ -79,7 +104,7 @@ def transfer():
         print(f'{cmdline} =========================== fail')
         print(stderr)
 
-    cmdline = "cleos get currency balance eosio.token zhangshiqi11"
+    cmdline = f"cleos get currency balance eosio.token {newaccount}"
     process = Popen(cmdline, stdout=PIPE, stderr=PIPE,shell=True)
     stdout, stderr = process.communicate()
     if stdout:
